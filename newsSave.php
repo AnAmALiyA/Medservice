fc2c63b186a89e7e2715bb85f1c0e13a04e34c9d
tt10is7 <tt10is7ios@gmail.com>
2017-09-24 18:08:43 +0300

    wp DB

--- newsSave.php
+++ newsSave.php
@@ -12,6 +12,18 @@ $post_data = array(
 );
 
 // Вставляем запись в базу данных
-$post_id = wp_insert_post( $post_data );
+wp_insert_post( $post_data );
 
+
+// Создаём объект записи
+  $my_post = array(
+     'post_title' => 'Название записи', // wp_strip_all_tags( $_POST['post_title'] )
+     'post_content' => 'А вот и текст. Ура, товарищи!', // $_POST['post_content']
+     'post_status' => 'publish',
+     'post_author' => 1,
+     'post_category' => array(8,39)
+  );
+
+// Вставляем запись в базу данных
+  wp_insert_post( $my_post );
 ?>
